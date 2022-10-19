fcdeb6b0d7269a0be246546f67c4d9ccbd9570c7
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-10-19 21:05:38 +0530

    find middle of a Linked list

---
+++ MidOfLL.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node *findMiddle(Node *head)
+{
+    Node *fast = head;
+    Node *slow = head;
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Node *head = new Node(n);
+        Node *t = head;
+        while (1)
+        {
+            // int t;
+            cin >> n;
+            if (n == -1)
+                break;
+            Node *temp = new Node(n);
+            t->next = temp;
+            t = t->next;
+        }
+        Node *mid = findMiddle(head);
+
+        cout << mid->data << endl;
+    }
+
+    return 0;
+}
