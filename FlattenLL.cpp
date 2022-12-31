033f5ade9cb0dcfd3a32eafe508048ec06e82c3d
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-31 22:12:44 +0530

    nothing

--- FlattenLL.cpp
+++ FlattenLL.cpp
@@ -23,13 +23,12 @@ Node *flattenLinkedList(Node *head)
     {
         if (current->child)
         {
-            // Find the end of the list
             Node *end = current->child;
             while (end->next)
             {
                 end = end->next;
             }
-            // Append the child list to the end of the current list
+
             end->next = current->next;
             current->next = current->child;
             current->child = NULL;
