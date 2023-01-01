529c27707e93e3f95205c178976e7611cf10811c
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-01 21:47:38 +0530

    Copy List with Random Pointer

---
+++ copyListRandomPointer.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class LinkedListNode
+{
+public:
+    T data;
+    LinkedListNode<T> *next;
+    LinkedListNode<T> *random;
+    LinkedListNode(T data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
