6e54efcd1931bdf920f5df1b733b6e453c4f81fe
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-06-29 15:40:38 +0530

    rotate 90 corrected

--- rotate90.cpp
+++ rotate90.cpp
@@ -1,18 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rotateMatrix(vector<vector<int>> &arr, int n, int m)
+void rotateMatrix(vector<vector<int>> &mat, int n, int m)
 {
-    for (int i = 0; i < n; i++)
+    // same as spiral print
+    int left = 0, right = m - 1, top = 0, bottom = n - 1;
+
+    while (left < right && top < bottom)
     {
-        for (int j = 0; j < i; j++)
-        {
-            swap(arr[i][j], arr[j][i]);
-        }
-    }
-    for (int i = 0; i < n; i++)
-    {
-        reverse(arr[i].begin(), arr[i].end());
+        int temp = mat[top][left];
+        for (int col = left + 1; col <= right; col++)
+            swap(mat[top][col], temp);
+        top++;
+
+        for (int row = top; row <= bottom; row++)
+            swap(mat[row][right], temp);
+        right--;
+
+        for (int col = right; col >= left; col--)
+            swap(mat[bottom][col], temp);
+        bottom--;
+
+        for (int row = bottom; row >= top; row--)
+            swap(mat[row][left], temp);
+        left++;
+
+        mat[top - 1][left - 1] = temp;
     }
 }
 
