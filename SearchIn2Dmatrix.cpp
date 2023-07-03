421872f1351cd86ff67a27a4fcbcc5821dacf542
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-07-03 16:22:20 +0530

    search in 2d matrix logn

--- SearchIn2Dmatrix.cpp
+++ SearchIn2Dmatrix.cpp
@@ -19,6 +19,31 @@ bool searchMatrix(vector<vector<int>> &mat, int t)
 {
     return findTargetInMatrix(mat, mat.size(), mat[0].size(), t);
 }
+bool searchMatrixNew(vector<vector<int>> &matrix, int target)
+{
+    int lo = 0;
+    if (!matrix.size())
+        return false;
+    int hi = (matrix.size() * matrix[0].size()) - 1;
+
+    while (lo <= hi)
+    {
+        int mid = (lo + (hi - lo) / 2);
+        if (matrix[mid / matrix[0].size()][mid % matrix[0].size()] == target)
+        {
+            return true;
+        }
+        if (matrix[mid / matrix[0].size()][mid % matrix[0].size()] < target)
+        {
+            lo = mid + 1;
+        }
+        else
+        {
+            hi = mid - 1;
+        }
+    }
+    return false;
+}
 
 int main()
 {
