26956678dee6b8eb3ec0300ed3fc06424f04f929
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-07-03 14:07:45 +0530

    hackMoney

--- hackMoney.cpp
+++ hackMoney.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool hackMoney(int money, int curr)
+{
+    if (money == 1)
+        return true;
+    if (curr == money)
+        return true;
+    if (curr > money)
+        return false;
+    return hackMoney(money, curr * 10) || hackMoney(money, curr * 20);
+}
+
 int main()
 {
     int t;
@@ -9,7 +20,7 @@ int main()
     {
         int n;
         cin >> n;
-        !(n % 10) || (n == 1) ? cout << "YES" : cout << "NO";
+        hackMoney(n, 1) ? cout << "YES" : cout << "NO";
         cout << endl;
     }
 
