1125cc3b163b53e2b3e8ca9d8a17e12d27a960de
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-07-10 15:47:11 +0530

    unique paths in grid

---
+++ uniquePathsCount.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int uniquePaths(int m, int n)
+{
+    int N = n + m - 2;
+    int r = m - 1;
+    double res = 1;
+
+    for (int i = 1; i <= r; i++)
+        res = res * (N - r + i) / i;
+    return (int)res;
+}
+
+int main()
+{
+
+    return 0;
+}
