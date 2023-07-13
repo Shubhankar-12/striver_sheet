24dfa4041deb7676a89b886d33b2a76af5d2bb33
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-07-13 18:08:53 +0530

    add two num as LL

---
+++ add2noLL.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *addTwoNumbers(Node *l1, Node *l2)
+{
+    Node *dum = new Node();
+    Node *temp = dum;
+    int carry = 0;
+    while (l1 != NULL || l2 != NULL || carry)
+    {
+        int sum = 0;
+        if (l1 != NULL)
+        {
+            sum += l1->data;
+            l1 = l1->next;
+        }
+        if (l2 != NULL)
+        {
+            sum += l2->data;
+            l2 = l2->next;
+        }
+        sum += carry;
+        carry /= 10;
+        Node *node = new Node(sum % 10);
+        temp->next = node;
+        temp = temp->next;
+    }
+    return dum;
+}
+
+int main()
+{
+
+    return 0;
+}
