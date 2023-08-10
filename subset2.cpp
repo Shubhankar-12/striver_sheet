595d7a4fb3d514aaddba26db03cb88001dd0f3c1
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-10 07:53:39 +0530

    subset 2

---
+++ subset2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void help(int ind, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ans)
+{
+    ans.push_back(ds);
+    for (int i = ind; i < nums.size(); i++)
+    {
+        if (i != ind && nums[i] == nums[i - 1])
+            continue;
+        ds.push_back(nums[i]);
+        help(i + 1, nums, ds, ans);
+        ds.pop_back();
+    }
+}
+
+vector<vector<int>> uniqueSubsets(int n, vector<int> &arr)
+{
+    vector<vector<int>> ans;
+    vector<int> ds;
+    sort(arr.begin(), arr.end());
+    help(0, arr, ds, ans);
+    return ans;
+}
+
+int main()
+{
+
+    return 0;
+}
