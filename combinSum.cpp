3e8bdcde97c2a7977c4e0dae8f82906d322e9426
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-10 08:25:54 +0530

    combination sum 1

--- combinSum.cpp
+++ combinSum.cpp
@@ -12,15 +12,21 @@ void help(vector<int> arr, int ind, int n, int k, vector<vector<int>> &ans, vect
         return;
     }
 
-    ref.push_back(arr[ind]);
-    help(arr, ind + 1, n, k, ans, ref);
-    ref.pop_back();
-
-    help(arr, ind + 1, n, k, ans, ref);
+    for (int i = ind; i < n; i++)
+    {
+        if (i > ind && arr[i] == arr[i - 1])
+            continue;
+        if (arr[i] > k)
+            break;
+        ref.push_back(arr[ind]);
+        help(arr, ind + 1, n, k, ans, ref);
+        ref.pop_back();
+    }
 }
 
 vector<vector<int>> findSubsetsThatSumToK(vector<int> arr, int n, int k)
 {
+    sort(arr.begin(), arr.end());
     vector<vector<int>> ans;
     vector<int> ref;
     help(arr, 0, n, k, ans, ref);
