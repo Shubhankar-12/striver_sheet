a7d538bca1dc4790ec020f9f91a1e45f1ed07be7
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-13 22:06:29 +0530

    nQueen rec using hashing

--- nQueen.cpp
+++ nQueen.cpp
@@ -1,43 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isSafe(int row, int col, vector<vector<int>> &board, int n)
-{
-
-    int dup_row = row;
-    int dup_col = col;
-    // first case
-    while (dup_col >= 0)
-    {
-        if (board[row][dup_col] == 1)
-            return false;
-        dup_col--;
-    } // second case-->first diagonal;
-    dup_col = col;
-    dup_row = row;
-    while (dup_col >= 0 && dup_row >= 0)
-    {
-        if (board[dup_row][dup_col] == 1)
-            return false;
-        dup_row--;
-        dup_col--;
-    } // third case-->second diagonal
-    ;
-    dup_col = col;
-    dup_row = row;
-    while (dup_col >= 0 && dup_row < n)
-    {
-        if (board[dup_row][dup_col] == 1)
-            return false;
-        dup_row++;
-        dup_col--;
-    }
-    return true;
-}
 void addBoard(vector<vector<int>> &board, vector<vector<int>> &res)
 {
     vector<int> temp;
     int n = board.size();
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -45,34 +13,52 @@ void addBoard(vector<vector<int>> &board, vector<vector<int>> &res)
             temp.push_back(board[i][j]);
         }
     }
+
     res.push_back(temp);
 }
-void solve(int col, vector<vector<int>> &board, vector<int> &flatten, vector<vector<int>> &ans, int n)
-{ // base case;
+
+void solve(int col, vector<int> &leftrow, vector<int> &upperDiagonal, vector<int> &lowerDiagonal, vector<vector<int>> &board, vector<vector<int>> &ans, int n)
+{
+
+    // base case;
     vector<int> temp;
     if (col == n)
     {
         addBoard(board, ans);
         return;
     }
+
     for (int row = 0; row < n; row++)
     {
-        if (isSafe(row, col, board, n))
+        if (leftrow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0)
         {
             board[row][col] = 1;
-            solve(col + 1, board, flatten, ans, n);
+            leftrow[row] = 1;
+            lowerDiagonal[row + col] = 1;
+            upperDiagonal[n - 1 + col - row] = 1;
+            solve(col + 1, leftrow, upperDiagonal, lowerDiagonal, board, ans, n);
             board[row][col] = 0;
+            leftrow[row] = 0;
+            lowerDiagonal[row + col] = 0;
+            upperDiagonal[n - 1 + col - row] = 0;
         }
     }
 }
+
 vector<vector<int>> solveNQueens(int n)
-{ // Write your code here.
+{
+
+    // Write your code here.
     vector<vector<int>> ans;
     vector<vector<int>> board(n, vector<int>(n, 0));
-    vector<int> flatten;
-    solve(0, board, flatten, ans, n);
+
+    vector<int> leftrow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
+
+    solve(0, leftrow, upperDiagonal, lowerDiagonal, board, ans, n);
+
     return ans;
 }
+
 int main()
 {
 
