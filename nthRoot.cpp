62c048793e3cba715c33d16f15d443d90e33052b
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-21 12:12:10 +0530

    nth root brute

--- nthRoot.cpp
+++ nthRoot.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int NthRoot(int n, int m)
+{
+    // Write your code here.
+    for (int i = 1; i <= m; i++)
+    {
+        if (pow(i, n) == m)
+            return i;
+        if (pow(i, n) > m)
+            return -1;
+    }
+}
+
 int main()
 {
 
