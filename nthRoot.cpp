736bcebc62d5cfe6d847afd35d41b884b979b7bd
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-21 12:40:43 +0530

    nth root optimal

--- nthRoot.cpp
+++ nthRoot.cpp
@@ -1,20 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int help(int mid, int n, int m)
+{
+    long long ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        ans = ans * mid;
+        if (ans > m)
+            return 2;
+    }
+    if (ans == m)
+        return 1;
+    return 0;
+}
+
 int NthRoot(int n, int m)
 {
     // Write your code here.
-    for (int i = 1; i <= m; i++)
+    int st = 1, end = m;
+    while (st <= end)
     {
-        if (pow(i, n) == m)
-            return i;
-        if (pow(i, n) > m)
-            return -1;
+        int mid = (st + end) / 2;
+        int midN = help(mid, n, m);
+        if (midN == 1)
+            return mid;
+        else if (midN == 0)
+            st = mid + 1;
+        else
+            end = mid - 1;
     }
+    return -1;
 }
 
 int main()
 {
-
+    int n, m;
+    cin >> n >> m;
+    cout << NthRoot(n, m);
     return 0;
 }
