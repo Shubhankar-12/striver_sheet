c3c9a370c74b9b87eed4119d3ec6b42f5ae88373
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-22 11:20:22 +0530

    single occ binary search

--- singleOccBinSer.cpp
+++ singleOccBinSer.cpp
@@ -3,13 +3,28 @@ using namespace std;
 
 int singleNonDuplicate(vector<int> &arr)
 {
-    // Write your code here
-    int ans = arr[0];
-    for (int i = 1; i < arr.size(); i++)
+    int n = arr.size();
+    if (n == 1)
+        return arr[0];
+    if (arr[0] != arr[1])
+        return arr[0];
+    if (arr[n - 1] != arr[n - 2])
+        return arr[n - 1];
+
+    int lo = 1, hi = n - 2;
+    while (lo <= hi)
     {
-        ans = ans ^ arr[i];
+        int mid = (lo + hi) >> 1;
+        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1])
+        {
+            return arr[mid];
+        }
+        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1]) || (mid % 2 == 0 && arr[mid] == arr[mid + 1]))
+            lo = mid + 1;
+        else
+            hi = mid - 1;
     }
-    return ans;
+    return -1;
 }
 
 int main()
