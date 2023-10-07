200226413fb26b9b68d9d94b0e1e2e660e0e5d1c
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-10-07 23:25:41 +0530

    min heap imp.

--- minHeap.cpp
+++ minHeap.cpp
@@ -1,116 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <bits/stdc++.h>
 
-int removemin(vector<int> &heap)
+long fun(vector<int> arr, int n)
 {
-
-    int ans = heap[0];
-
-    heap[0] = heap[heap.size() - 1];
-
-    heap.pop_back();
-
-    int pi = 0;
-
-    int n = heap.size();
-
-    int minindx;
-
-    while (pi < n)
+    double s = 0;
+    for (int i = 0; i < n; i++)
     {
-
-        int lci = 2 * pi + 1;
-
-        int rci = 2 * pi + 2;
-
-        if (lci < n && rci < n)
-        {
-
-            if (heap[lci] < heap[rci])
-                minindx = lci;
-
-            else
-                minindx = rci;
-        }
-
-        else if (lci < n && rci >= n)
-        {
-
-            minindx = lci;
-        }
-        else
-            break;
-
-        if (heap[minindx] < heap[pi])
-        {
-
-            swap(heap[minindx], heap[pi]);
-        }
-        else
-            break;
-
-        pi = minindx;
+        s += (double)arr[i];
     }
-
-    return ans;
+    // (s+x)/(n+1)=2(s/2);
+    double x = ((2 * (s / (double)n)) * (double)(n + 1)) - s;
+    return ceil(x);
 }
 
-void insertinheap(vector<int> &heap, int ele)
-{
-
-    heap.push_back(ele);
-
-    int ci = heap.size() - 1;
-
-    while (ci > 0)
-    {
-
-        int pi = (ci - 1) / 2;
-
-        if (heap[ci] < heap[pi])
-        {
-
-            swap(heap[ci], heap[pi]);
-        }
-        else
-        {
-
-            break;
-        }
-
-        ci = pi;
-    }
-}
-
-vector<int> minHeap(int n, vector<vector<int>> &q)
-{
-
-    vector<int> heap;
-
-    vector<int> ans;
-
-    for (int i = 0; i < q.size(); i++)
-    {
-
-        if (q[i][0] == 0)
-        {
-
-            insertinheap(heap, q[i][1]);
-        }
-        else
-        {
-
-            int removed = removemin(heap);
-
-            ans.push_back(removed);
-        }
-    }
-
-    return ans;
-}
 int main()
 {
-
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << fun(arr, n);
     return 0;
 }
