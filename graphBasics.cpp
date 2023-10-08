8911ca96ae28843286a5f469c98629e0bce1ee0f
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-10-08 23:28:07 +0530

    graph dfs

--- graphBasics.cpp
+++ graphBasics.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void dfs(int node, vector<int> &vis, vector<int> adj[], vector<int> &tmp)
+{
+    vis[node] = 1;
+    tmp.push_back(node);
+    for (auto adj_node : adj[node])
+    {
+        if (!vis[adj_node])
+        {
+            dfs(adj_node, vis, adj, tmp);
+        }
+    }
+}
+
+vector<vector<int>> depthFirstSearch(int n, int e, vector<vector<int>> &edges)
+{
+    vector<int> adj[n + 1];
+    for (int i = 0; i < e; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<vector<int>> ans;
+    vector<int> vis(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            vector<int> tmp;
+            dfs(i, vis, adj, tmp);
+            ans.push_back(tmp);
+        }
+    }
+    return ans;
+}
 int main()
 {
     int n, m;
