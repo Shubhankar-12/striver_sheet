dc00bc84594fef9a6c345e3fdf69edbb03cf7a3f
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-11-02 23:51:46 +0530

    floyd warshall cycle detection

--- floydWarshall.cpp
+++ floydWarshall.cpp
@@ -25,6 +25,8 @@ public:
                 for (int j = 0; j < n; j++)
                 {
                     mat[i][j] = min(mat[i][j], mat[i][k] + mat[k][j]);
+                    if (i == j && mat[i][j] < 0)
+                        return;
                 }
             }
         }
